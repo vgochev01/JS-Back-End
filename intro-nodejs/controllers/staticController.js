d663dd28d4dbe4f0968c7b1c0a4e8cd513dfe652
Valeri <csvalkata@gmail.com>
2021-05-22 19:12:17 +0300

    static files content types

--- intro-nodejs/controllers/staticController.js
+++ intro-nodejs/controllers/staticController.js
@@ -2,11 +2,36 @@ const fs = require('fs');
 
 module.exports = (req, res) => {
     const filename = req.url.slice(8);
-    const data = fs.createReadStream(`./static/${filename}`);
-    data.on('error', () => {
+    const file = fs.createReadStream(`./static/${filename}`);
+
+    let type = '';
+
+    if(filename.endsWith('css')){
+        type = 'text/css';
+    } else if(filename.endsWith('jpg') || filename.endsWith('jpeg')){
+        type = 'image/jpeg';
+    } else if(filename.endsWith('png')){
+        type = 'image/png';
+    }
+
+    file.on('error', () => {
         res.statusCode = 404;
         res.write('Not Found');
         res.end();
     })
-    data.pipe(res);
+
+    file.once('data', data => {
+        res.writeHead(200, {
+            'Content-Type': type
+        });
+        send(data);
+        file.on('data', send);
+    });
+
+    file.on('end', () => res.end());
+
+
+    function send(data){
+        res.write(data);
+    }
 }
