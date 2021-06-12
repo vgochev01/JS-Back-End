ab48a0c73f2dc2ed51009b5c46bbf409cc638b1f
Valeri <csvalkata@gmail.com>
2021-06-12 15:15:37 +0300

    user service - register done

--- authentication/Workshop_Cubicle_3/config/routes.js
+++ authentication/Workshop_Cubicle_3/config/routes.js
@@ -1,15 +1,14 @@
-const { init } = require("./storage");
 
 const productController = require('../controllers/productController');
 const accessoryController = require('../controllers/accessoryController');
+const authController = require('../controllers/authController');
 const commonController = require('../controllers/commonController');
 
 module.exports = (app) => {
-
-  app.use(init());
   
   app.use('/products', productController);
   app.use('/accessory', accessoryController);
+  app.use('/auth', authController);
   app.use('/', commonController);
 
 };
